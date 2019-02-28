1441e2f43d15831cc737c0f0144dbdafa7b06164
Alena Koroliova <47770796+9ElenaKoroliova@users.noreply.github.com>
2019-02-28 22:14:29 +0300

    Update for task1.py

--- week0/task1.py
+++ week0/task1.py
@@ -1,19 +1,19 @@
-c = int (input())
-a = 1
-b = 1
-g = 0
-while a <= c:
-    for b in range(1, c):
-         if a ** 2 + b ** 2 == c:
-             g+=1
-             break
-         else:
-             b = b + 1
-    if g == 1:
-        break
-    a += 1
-
-if  g == 0:
-    print(False)
-else:
-    print(True)
+class Solution(object):
+    def judgeSquareSum(self, c):
+     a = 0
+     b = 0
+     g = 0
+     while a <= c:
+        for b in range(1, c):
+            if a ** 2 + b ** 2 == c:
+                g += 1
+                break
+            else:
+                b = b + 1
+        if g == 1:
+            break
+        a += 1
+     return g != 0
+if __name__ == "__main__":
+    n = int(input())
+    print(Solution().judgeSquareSum(n))
